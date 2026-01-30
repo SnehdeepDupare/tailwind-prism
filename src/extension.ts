9f4335b89c21f659c162a3d8077ed840acc0ea68
Snehdeep <duparesnehdeep@gmail.com>
2026-01-31 00:41:41 +0530

    feat: add toggle to enable/disable the highlight

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,144 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let enabled = false;
+let decorations: vscode.TextEditorDecorationType[] = [];
+
 export function activate(context: vscode.ExtensionContext) {
+  const toggle = vscode.commands.registerCommand(
+    "tailwind-prism.toggle",
+    () => {
+      enabled = !enabled;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tailwind-prism" is now active!');
+      if (!enabled) {
+        clearDecorations();
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tailwind-prism.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Tailwind Prism!');
-	});
+      updateEditor(vscode.window.activeTextEditor);
+    },
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(toggle);
+
+  vscode.window.onDidChangeActiveTextEditor(updateEditor);
+  vscode.workspace.onDidChangeTextDocument(() =>
+    updateEditor(vscode.window.activeTextEditor),
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function updateEditor(editor?: vscode.TextEditor) {
+  if (!enabled || !editor) return;
+
+  clearDecorations();
+
+  const text = editor.document.getText();
+
+  const classRegex = /(className|class)\s*=\s*"([^"]+)"/g;
+
+  const variantRanges: vscode.Range[] = [];
+  const importantRanges: vscode.Range[] = [];
+  const arbitraryRanges: vscode.Range[] = [];
+  const utilityRanges: vscode.Range[] = [];
+
+  let match: RegExpExecArray | null;
+
+  while ((match = classRegex.exec(text))) {
+    const classValue = match[2];
+    const baseIndex = match.index + match[0].indexOf(classValue);
+
+    const parts = classValue.split(/\s+/);
+    let offset = 0;
+
+    for (const part of parts) {
+      if (!part) continue;
+
+      const start = baseIndex + offset;
+      const startPos = editor.document.positionAt(start);
+
+      // variant: hover:, dark:, sm:
+      const variantMatch = part.match(/^([a-z-]+:)/);
+      if (variantMatch) {
+        variantRanges.push(
+          new vscode.Range(
+            startPos,
+            startPos.translate(0, variantMatch[1].length),
+          ),
+        );
+      }
+
+      // important: !
+      if (part.startsWith("!")) {
+        importantRanges.push(
+          new vscode.Range(startPos, startPos.translate(0, 1)),
+        );
+      }
+
+      // arbitrary: [state=open]
+      const arbitraryMatch = part.match(/\[[^\]]+\]/);
+      if (arbitraryMatch) {
+        const i = part.indexOf(arbitraryMatch[0]);
+        arbitraryRanges.push(
+          new vscode.Range(
+            startPos.translate(0, i),
+            startPos.translate(0, i + arbitraryMatch[0].length),
+          ),
+        );
+      }
+
+      // utility fallback
+      utilityRanges.push(
+        new vscode.Range(startPos, startPos.translate(0, part.length)),
+      );
+
+      offset += part.length + 1;
+    }
+  }
+
+  applyDecorations(editor, {
+    variantRanges,
+    importantRanges,
+    arbitraryRanges,
+    utilityRanges,
+  });
+}
+
+function applyDecorations(
+  editor: vscode.TextEditor,
+  ranges: {
+    variantRanges: vscode.Range[];
+    importantRanges: vscode.Range[];
+    arbitraryRanges: vscode.Range[];
+    utilityRanges: vscode.Range[];
+  },
+) {
+  const variant = vscode.window.createTextEditorDecorationType({
+    color: "#7FB4FF",
+    fontStyle: "italic",
+  });
+
+  const important = vscode.window.createTextEditorDecorationType({
+    color: "#FF6B81",
+    fontWeight: "600",
+  });
+
+  const arbitrary = vscode.window.createTextEditorDecorationType({
+    color: "#F2C97D",
+  });
+
+  const utility = vscode.window.createTextEditorDecorationType({
+    color: "#D1D7E0",
+    fontWeight: "500",
+  });
+
+  decorations.push(variant, important, arbitrary, utility);
+
+  editor.setDecorations(variant, ranges.variantRanges);
+  editor.setDecorations(important, ranges.importantRanges);
+  editor.setDecorations(arbitrary, ranges.arbitraryRanges);
+  editor.setDecorations(utility, ranges.utilityRanges);
+}
+
+function clearDecorations() {
+  decorations.forEach((d) => d.dispose());
+  decorations = [];
+}
