ac8fc167a585acd2a6046421d342809a69264323
Snehdeep <duparesnehdeep@gmail.com>
2026-02-02 18:19:32 +0530

    feat: add comparison section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,25 +1,26 @@
+import { Compare } from "@/components/compare";
+import { Hero } from "@/components/hero";
+
 export default function Home() {
   return (
-    <div className="flex-1">
-      <div className="px-5 pt-20">
-        <h1 className="text-3xl font-bold tracking-tight text-neutral-900 md:text-5xl">
-          Make Tailwind classes readable again.
-        </h1>
-        <p className="text-muted-foreground mt-4 max-w-4xl text-base">
-          Semantic syntax highlighting for your Tailwind CSS classes, so you
-          don&apos;t get confused again and edit long classes without slowing
-          down.
-        </p>
+    <div className="flex-1 pb-20">
+      <Hero />
 
-        <div className="mt-4 flex gap-2">
-          <button className="bg-primary text-primary-foreground cursor-pointer rounded-md px-4 py-2 text-sm font-medium">
-            Install from VS Code Marketplace
-          </button>
-          <button className="cursor-pointer rounded-md bg-neutral-100 px-4 py-2 text-sm font-medium">
-            View on GitHub
-          </button>
+      <section className="mt-10 px-5">
+        <div className="flex items-center justify-center rounded-[30px] border-4 border-[#6C6C6C] bg-[#222222] p-4 md:p-6">
+          <div className="flex h-full w-full items-center justify-center rounded-2xl bg-gray-100 p-2 md:p-4">
+            <Compare
+              firstImage="/before.png"
+              secondImage="/after.png"
+              firstImageClassName="object-cover object-left-top"
+              secondImageClassname="object-cover object-left-top"
+              className="h-[250px] w-full md:h-[500px] md:w-full"
+              slideMode="hover"
+              autoplay
+            />
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
---
+++ apps/web/components/hero.tsx
@@ -0,0 +1,23 @@
+export const Hero = () => {
+  return (
+    <section className="px-5 pt-20">
+      <h1 className="text-3xl font-bold tracking-tight text-neutral-900 md:text-5xl">
+        Make Tailwind classes readable again.
+      </h1>
+      <p className="text-muted-foreground mt-4 max-w-2xl text-base">
+        Semantic syntax highlighting for your Tailwind CSS classes, so you
+        don&apos;t get confused again and edit long classes without slowing
+        down.
+      </p>
+
+      <div className="mt-4 flex gap-2">
+        <button className="bg-primary text-primary-foreground cursor-pointer rounded-md px-4 py-2 text-sm font-medium">
+          Install from VS Code Marketplace
+        </button>
+        <button className="cursor-pointer rounded-md bg-neutral-100 px-4 py-2 text-sm font-medium">
+          View on GitHub
+        </button>
+      </div>
+    </section>
+  );
+};
