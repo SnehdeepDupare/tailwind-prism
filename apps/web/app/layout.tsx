b11bef2a3e45d58415479778e5dee56e46a85a3c
Snehdeep <duparesnehdeep@gmail.com>
2026-02-04 23:23:21 +0530

    feat: add vercel analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { siteConfig } from "@/constants/site";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -80,7 +81,7 @@ export default function RootLayout({
           <Header />
 
           {children}
-
+          <Analytics />
           <Footer />
         </main>
       </body>
